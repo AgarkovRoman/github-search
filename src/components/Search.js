5d77b58b255c0ec46562f9379bb370e8ca0af06f
Roman Agarkov <agarkovromachka@mail.ru>
2020-06-12 10:09:55 +0300

    add main pages

---
+++ src/components/Search.js
@@ -0,0 +1,33 @@
+import React, { useContext, useState } from 'react'
+import { AlertContext } from '../context/alert/alertContext'
+
+export const Search = () => {
+    const [value, setValue] = useState('')
+    const { show } = useContext(AlertContext)
+
+    const onSubmit = event => {
+        if (event.key !== 'Enter') {
+            return
+        }
+        if (value.trim()) {
+            console.log('make reques')
+        } else {
+            show('Введите данные пользователя!')
+        }
+    }
+
+    return (
+        <div>
+            <input
+                type='text'
+                className='form-control mb-4'
+                placeholder='Введите ник пользователя'
+                value={value}
+                onChange={event => setValue(event.target.value)}
+                onKeyPress={onSubmit}
+            />
+        </div>
+    )
+}
+
+
